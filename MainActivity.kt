6269639e5a99de124c35c50d04c9e508dcfd31de
alamdarhussain-eng <alamdar.hussain@seecs.edu.pk>
2026-01-06 15:33:16 +0500

    Create MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.myapsscriptapp
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val webView = WebView(this)
+        setContentView(webView)
+
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("https://script.google.com/macros/s/AKfycbwgHeIWRShpbBV_Sbmmn9h-RctF0gY5ZzSGIL-ZfBJbYznkoRSJx3MPhjLnOdA4Ey5Jnw/exec")
+    }
+}
